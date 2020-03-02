eeac156bac62feb28fc9dc3721c08e91295bced9
17.04401-4 <17.04401-4@maua.br>
2020-03-02 11:26:11 -0300

    Meu Primeiro Programa

---
+++ Linguagem1_Projetos/Praticas01/Aula1/MeuPrograma.java
@@ -0,0 +1,9 @@
+class MeuPrograma {
+    public static void main(String[] args) {
+
+        // Mensagem de saida do sistema
+        System.out.println("Minha Primeira Aplicação em JAVA!");
+        System.out.println("Olá Mundo!");
+        System.out.println("Estou programando no modo Conan!");
+    }
+}
