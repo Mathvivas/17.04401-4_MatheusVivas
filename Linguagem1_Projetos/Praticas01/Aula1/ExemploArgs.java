04f485de55b0ffc801ac98c77a050fd3dbb1d82a
17.04401-4 <17.04401-4@maua.br>
2020-03-02 11:42:30 -0300

    Arquivo Exemplo de Argumentos

---
+++ Linguagem1_Projetos/Praticas01/Aula1/ExemploArgs.java
@@ -0,0 +1,10 @@
+class ExemploArgs {
+    public static void main (String[] args) {       // Vetor de strings com o nome args
+
+        int i;
+        for(i = 0; i < args.length; i++) {
+            System.out.println( args[i] );
+        }
+
+    }
+}
